e8ce7b73604c0f20c152a512439ee6f085e2b058
SulmaDubon <suldubon@gmail.com>
2024-10-26 15:28:45 +0200

    Forzar despliegue en Render

--- core/settings.py
+++ core/settings.py
@@ -1,22 +1,31 @@
-
 from pathlib import Path
 import os
 import environ
-from django.contrib.messages import constants as messages
 import dj_database_url
+from django.contrib.messages import constants as messages
 
-env= environ.Env()
+# Inicializa las variables de entorno
+env = environ.Env()
 environ.Env.read_env()
 
-
+# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# Configuración de la clave secreta y el modo DEBUG
+SECRET_KEY = env('SECRET_KEY', default="default-secret-key")  # Agrega una clave secreta por defecto solo para pruebas locales
+DEBUG = env.bool('DEBUG', default=False)
 
-# Application definition
+# Configuración de los hosts permitidos
+ALLOWED_HOSTS = [os.getenv('RENDER_EXTERNAL_HOSTNAME', '*')]
 
+# Configuración de la base de datos usando DATABASE_URL
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL", 'postgres://postgres:kahuOpilikia@localhost:5432/mi_basedatos')
+    )
+}
+
+# Aplicaciones instaladas
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -35,11 +44,9 @@ INSTALLED_APPS = [
     'users',
     'dashboard_users',
     'admin_panel',
-   
-    
-    
 ]
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # Habilitado para archivos estáticos
@@ -52,12 +59,21 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+# Configuración para servir archivos estáticos con WhiteNoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# URL y ruta de archivos estáticos
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'dashboard_users' / 'static',  # Archivos estáticos adicionales en la app 'dashboard_users'
+]
 
+# Configuración de URLs y WSGI
 ROOT_URLCONF = 'core.urls'
+WSGI_APPLICATION = 'core.wsgi.application'
 
+# Configuración de plantillas
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -74,28 +90,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'nombre_por_defecto'),
-        'USER': os.environ.get('DB_USER', 'usuario_por_defecto'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'contraseña_por_defecto'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
-}
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
+# Configuración de validación de contraseñas
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -111,40 +106,24 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
+# Configuración de localización
 LANGUAGE_CODE = 'es'
 TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-
-# Archivos estáticos
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Aquí se recolectan todos los archivos estáticos después de ejecutar collectstatic
-STATICFILES_DIRS = [
-    BASE_DIR / 'dashboard_users' / 'static',  # Aquí Django buscará archivos estáticos adicionales en la app 'dashboard_users'
-]
-
-
-
-# Default primary key field type
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if not DEBUG: 
+# Configuración de backend de correo
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'  # Reemplaza 'smtp.example.com' con tu servidor SMTP
-    EMAIL_PORT = 587  # Puerto SMTP (generalmente 587 para TLS/STARTTLS o 465 para SSL)
-    EMAIL_USE_TLS = True  # Usar TLS para la conexión (True o False)
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    
- 
-# Configuración de mensajes
 
+# Configuración de mensajes
 MESSAGE_TAGS = {
     messages.DEBUG: 'debug',
     messages.INFO: 'info',
@@ -152,11 +131,14 @@ MESSAGE_TAGS = {
     messages.WARNING: 'warning',
     messages.ERROR: 'error',
 }
-AUTH_USER_MODEL = 'users.CustomUser'
 
+# Configuración de modelo de usuario y URLs de inicio de sesión
+AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'dashboard_users:dashboard'
 LOGOUT_REDIRECT_URL = 'home'
 
+# Configuración de campo de clave primaria predeterminado
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
